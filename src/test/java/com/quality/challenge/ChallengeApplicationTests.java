2d1c934fcf2ba7ddec1f12c1dd3073be39797179
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-03-16 09:46:46 -0300

    feat: add Spring initial configurations

---
+++ src/test/java/com/quality/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quality.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
