b71482f0151cc732737914f643a8997630cede43
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-03-18 10:37:33 -0300

    feat: add unit tests for hotels repository

--- src/test/java/com/quality/challenge/HotelRepositoryTests.java
+++ src/test/java/com/quality/challenge/HotelRepositoryTests.java
@@ -1,15 +1,19 @@
 package com.quality.challenge;
 
 import com.quality.challenge.dto.HotelDTO;
+import com.quality.challenge.exceptions.InvalidDestinationException;
 import com.quality.challenge.exceptions.ServerErrorException;
 import com.quality.challenge.interfaces.IHotelRepository;
 import com.quality.challenge.repository.HotelRepository;
+import com.quality.challenge.utils.StatusCodeUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 class HotelRepositoryTests {
     private IHotelRepository hotelRepository;
@@ -44,5 +48,47 @@ class HotelRepositoryTests {
         Assertions.assertNotEquals(allHotels, result);
         Assertions.assertNotEquals(allHotels.size(), result.size());
         Assertions.assertTrue(allHotels.size() > result.size());
+    }
+
+    @Test
+    void containsDestinationSuccess(){
+        // Act & Assert
+        Assertions.assertDoesNotThrow(() -> this.hotelRepository.containsDestination("puerto iguazú"));
+    }
+
+    @Test
+    void containsDestinationThrowsInvalidDestinationException(){
+        // Arrange
+        InvalidDestinationException expectedException = new InvalidDestinationException(StatusCodeUtil.getCustomStatusCode(
+                "The chosen destination does not exist",
+                HttpStatus.NOT_FOUND));
+
+        // Act & Assert
+        InvalidDestinationException thrownException = Assertions.assertThrows(
+                InvalidDestinationException.class,
+                () -> this.hotelRepository.containsDestination("invalid")
+        );
+        Assertions.assertEquals(expectedException.getMessage(), thrownException.getMessage());
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, thrownException.getStatusCodeDTO().getStatus());
+    }
+
+    @Test
+    void hasAvailabilityWithPresentResult(){
+        // Act
+        Optional<HotelDTO> result = this.hotelRepository.hasAvailability("CH-0002", "puerto iguazú", new Date("02/11/2021"), new Date("03/19/2021"), "double");
+
+        // Assert
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    void hasAvailabilityWithEmptyResult(){
+        // Act
+        Optional<HotelDTO> result = this.hotelRepository.hasAvailability("foo", "bar", new Date("02/11/2021"), new Date("03/19/2021"), "baz");
+
+        // Assert
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.isEmpty());
     }
 }
